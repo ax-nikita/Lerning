49204dc74da2e96c6088e441e704f9e6b0a60fa1
Nikita Zonov <zonovnik63@gmail.com>
2026-08-21 18:32:25 +0300

    Добавлен pest и перенесен один тест из дня 5 PHP

---
+++ php/day2/tests/Unit/Application/CreateTicketServiceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Ticket\InMemoryTicketRepository;
+use App\Ticket\TicketId;
+use App\Ticket\TicketPriority;
+use App\Ticket\TicketStatus;
+
+use App\Application\CreateTicketService;
+
+use App\Clock\FixedClock;
+
+test('Create Ticket full Happy Test', function () {
+    $title = 'Тест';
+    $priority = TicketPriority::High;
+    $id = new TicketId(bin2hex(random_bytes(16)));
+    $status = TicketStatus::New;
+
+    $createdAt = new \DateTimeImmutable('2026-08-06 15:08:24');
+    $repository = new inMemoryTicketRepository();
+    $clock = new FixedClock($createdAt);
+
+    $service = new CreateTicketService($repository, $clock);
+
+    $ticket = $service->create($title, $priority, $id, $status);
+
+    // Test ticket fields
+    expect($ticket->createdAt)->toBe($createdAt);
+    expect($ticket->title)->toBe($title);
+    expect($ticket->status)->toBe($status);
+    expect($ticket->priority)->toBe($priority);
+    expect($ticket->id)->toBe($id);
+    expect($ticket->assigneeId)->toBe(null);
+
+    // Test ticket exist in repository
+    expect($repository->find($id))->toBe($ticket);
+
+    // part two, test assigneeId
+    $id = new TicketId(bin2hex(random_bytes(16)));
+
+    $assigneeId = 12;
+    $ticket = $service->create($title, $priority, $id, $status, $assigneeId);
+
+    expect($ticket->assigneeId)->toBe($assigneeId);
+});
