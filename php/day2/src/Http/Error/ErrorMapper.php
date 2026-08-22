eb73ce9c23ad84c4a93babca48168228e5bb0243
Nikita Zonov <zonovnik63@gmail.com>
2026-08-22 17:34:05 +0300

    Убран свитч в пользу более гибкого match

--- php/day2/src/Http/Error/ErrorMapper.php
+++ php/day2/src/Http/Error/ErrorMapper.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Error;
 
@@ -8,33 +9,27 @@ use App\Ticket\TicketAlreadyExistsException;
 
 final class ErrorMapper
 {
-    function map(\Throwable $exception): HttpResponse
+    public function map(\Throwable $exception): HttpResponse
     {
-        switch ($exception::class) {
-            case RequestValidationException::class:
-                return new HttpResponse(422, [
-                    'error' => [
-                        'code' => ErrorCode::ValidationFailed->value,
-                        'message' => $exception->getMessage(),
-                    ]
-                ]);
-                break;
-            case TicketAlreadyExistsException::class:
-                return new HttpResponse(409, [
-                    'error' => [
-                        'code' => ErrorCode::TicketAlreadyExists->value,
-                        'message' => $exception->getMessage(),
-                    ]
-                ]);
-                break;
-            default:
-                return new HttpResponse(500, [
-                    'error' => [
-                        'code' => ErrorCode::InternalError->value,
-                        'message' => 'Internal server error',
-                    ]
-                ]);
-                break;
-        }
+        return match (true) {
+            $exception instanceof RequestValidationException => new HttpResponse(422, [
+                'error' => [
+                    'code' => ErrorCode::ValidationFailed->value,
+                    'message' => $exception->getMessage(),
+                ]
+            ]),
+            $exception instanceof TicketAlreadyExistsException => new HttpResponse(409, [
+                'error' => [
+                    'code' => ErrorCode::TicketAlreadyExists->value,
+                    'message' => $exception->getMessage(),
+                ]
+            ]),
+            $exception instanceof \Exception => new HttpResponse(500, [
+                'error' => [
+                    'code' => ErrorCode::InternalError->value,
+                    'message' => 'Internal server error',
+                ]
+            ])
+        };
     }
 }
